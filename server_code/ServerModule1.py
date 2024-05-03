d3202877cb5671c3517c92f2668e0357b5d21ef9
developper@amela-po.com <developper@amela-po.com>
2024-05-03 07:50:45 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -14,7 +14,7 @@ def get_data(country, street_name, house_number, city, zip_code, roof_area):
     file = anvil.server.get_asset('3-EM_consumption_data_template_Coatinc_voorbeeld.xlsx')
     
     # Read the CSV file into a pandas DataFrame
-    df = pd.read_excel(file)
+    df = pd.read_excel(file, sheet_name='Electricity')
     
     # Optionally, perform any data manipulation here if needed
     # For example, you might want to convert dates, fill NaNs, etc.
@@ -22,13 +22,13 @@ def get_data(country, street_name, house_number, city, zip_code, roof_area):
     # Return the DataFrame to the client
     return df.to_dict('records')
   
-  address = house_number + ' ' + street_name + ' ' + city
-  
-  pv_output = anvil.server.call('calculate_solar_radiation_1year', address, roof_area)
-  yearly_pv_output = pv_output[0][0]
-  monthly_pv_output = pv_output[1]
-  energy_calc_output_text = "Your yearly solar energy potential: " \
-                          + str(int(yearly_pv_output)) + " kWh" \
-                          + "\n \n" + "Check out your expected monthly electricity production below!"
-  
-  return [energy_calc_output_text, monthly_pv_output]
+    # address = house_number + ' ' + street_name + ' ' + city
+    
+    # pv_output = anvil.server.call('calculate_solar_radiation_1year', address, roof_area)
+    # yearly_pv_output = pv_output[0][0]
+    # monthly_pv_output = pv_output[1]
+    # energy_calc_output_text = "Your yearly solar energy potential: " \
+    #                         + str(int(yearly_pv_output)) + " kWh" \
+    #                         + "\n \n" + "Check out your expected monthly electricity production below!"
+    
+    # return [energy_calc_output_text, monthly_pv_output]
